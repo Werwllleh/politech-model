ea0cebb0882dacd710d9467bc25f1eacb8133c1d
Werw <allvxm4@gmail.com>
2023-03-05 19:02:41 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React, {Suspense} from 'react';
 import './App.css';
-
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import {Model} from "./Model";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Canvas className='canvas4'>
+          <OrbitControls autoRotate={true} enableZoom={true} />
+          <ambientLight intensity={0.5}/>
+          <directionalLight position={[-2,5,3]} intensity={1}/>
+          <group>
+            <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position = {[0, -1.1, -2]}>
+              <planeBufferGeometry attach='geometry' args={[10, 10]}/>
+              <meshStandardMaterial attach='material' color='#ededed'/>
+            </mesh>
+          </group>
+          <Suspense fallbak={null}>
+            <Model/>
+          </Suspense>
+        </Canvas>
+      </div>
   );
 }
 
